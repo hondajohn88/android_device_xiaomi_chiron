056354e92b53447da864325118fe92e8b5bbdfa1
hondajohn88 <hondajohn88@gmail.com>
2018-03-22 20:54:25 -0700

    Try to get camera working.

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -14,6 +14,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
@@ -25,11 +26,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.memtrack@1.0-impl \
     android.hardware.graphics.composer@2.1-impl \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.0_vendor
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # DRM
 PRODUCT_PACKAGES += \
