ff914897addd5a5ad6cb9284e7da49040a7d588f
hondajohn88 <hondajohn88@gmail.com>
2018-05-16 16:30:47 -0700

    Update to use msm8998-common.

--- full_chiron.mk
+++ full_chiron.mk
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,7 +23,7 @@ $(call inherit-product, device/xiaomi/chiron/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := full_chiron
-PRODUCT_DEVICE := chiron 
+PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := MI Mix 2
+PRODUCT_MODEL := Mi MIX 2
 PRODUCT_MANUFACTURER := Xiaomi
