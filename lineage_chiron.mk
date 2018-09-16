93ff9dae2598a8790be6b65901178a7d8440977e
hondajohn88 <hondajohn88@gmail.com>
2018-09-15 17:20:04 -0700

    Remove duplicate .mk

--- lineage_chiron.mk
+++ lineage_chiron.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/xiaomi/chiron/full_chiron.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit aos stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
